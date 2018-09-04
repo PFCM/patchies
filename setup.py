15925c0fbb2038b0d6919ce83e066b39470394a2
pfcm <pfcmathews@gmail.com>
2018-09-04 17:15:40 +1200

    improve setup

--- setup.py
+++ setup.py
@@ -10,10 +10,14 @@ def read_file(path):
 
 setup(
     name='patchies',
-    version='0.1',
+    version='0.1.1',
     packages=['patchies'],
     install_requires=read_file('requirements.txt').split('\n'),
     entry_points='''
         [console_scripts]
         patchies=patchies.pipeline:cli
-        ''')
+        ''',
+    descriptions='Fast Approximate Nearest Neighbour for making silly pictures',
+    long_description=read_file('README.md'),
+    author='pfcm',
+    url='https://github.com/pfcm/patchies')
